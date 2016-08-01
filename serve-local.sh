34b95564011c0ab928586c49c1a31e851e8ad62b
Hilmar Lapp <hlapp@drycafe.net>
2016-07-31 23:48:43 -0400

    Disables rebuilding on each invocation.
    This needs to be moved to its own script.

--- serve-local.sh
+++ serve-local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t phyloref/jekyll .
-docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 phyloref/jekyll
+# docker build --pull -t phyloref/jekyll .
+docker run --rm --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 phyloref/jekyll
