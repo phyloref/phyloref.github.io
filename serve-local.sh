ee20044f971cb6cdfd2540eadd982ba906130fd6
Hilmar Lapp <hlapp@drycafe.net>
2018-05-14 12:01:18 -0400

    Updates jekyll containers to default jekyll/jekyll

--- serve-local.sh
+++ serve-local.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# docker build --pull -t phyloref/jekyll .
-docker run --rm --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 phyloref/jekyll jekyll serve --config _config.yml,_config-dev.yml --drafts
+docker run --rm --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 jekyll/jekyll jekyll serve --config _config.yml,_config-dev.yml --drafts
